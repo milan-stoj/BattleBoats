2af1e6459404da5373d9c0512a6d1e14898788f2
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-07-20 15:54:57 -0500

    init game class for logic

---
+++ BattleShip/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Game
+    {
+        Player player1;
+        Player player2;
+        Board player1Board;
+        Board player2Board;
+
+        public Game()
+        {
+            //
+        }
+
+    }
+}
